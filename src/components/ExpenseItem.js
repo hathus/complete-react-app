d20852190e612f90d416264df5d35467df6013e9
Hathus <trilogy@gmail.com>
2022-04-29 20:48:04 -0500

    format the date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('es-MX', { month: 'short'}).toUpperCase();
+  const day = props.date.toLocaleString('es-MX', { day: '2-digit' });
+  const year = props.date.getFullYear();
   return (
     <div className='expense-item'>
-      <div>{props.date.toUTCString()}</div>
+      <div className='expense-item__price'>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
